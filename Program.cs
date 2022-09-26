76fdaefe7b64456f6bec42358e9ce78702772623
OldBeliver <380506338004@yandex.com>
2022-09-26 15:24:14 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Pictures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countInLine = 3;
+            int allPictures = 52;
+
+            int completedLines;
+            int rest;
+
+            completedLines = allPictures / countInLine;
+            rest = allPictures % countInLine;
+
+            Console.WriteLine($"если {allPictures} картинки разложить по {countInLine} в ряд, то получится {completedLines} рядов и один неполный ряд с {rest} картинками");
+        }
+    }
+}
